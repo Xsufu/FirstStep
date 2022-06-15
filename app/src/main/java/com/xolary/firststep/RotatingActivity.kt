911262468137a9549d6ae6c8c7951d6cd09f7973
Xsufu <aluno.macarrao@gmail.com>
2022-06-15 16:03:29 +0700

    Добавлена новая активность - RotatingActivity

---
+++ app/src/main/java/com/xolary/firststep/RotatingActivity.kt
@@ -0,0 +1,11 @@
+package com.xolary.firststep
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RotatingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rotating)
+    }
+}
